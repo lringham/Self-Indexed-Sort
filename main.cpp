83fd3926d1b0c67a0d4eb1515c905c965cb99f06
lee ringham <leeringham@gmail.com>
2019-03-14 23:15:07 -0600

    cleaned up code, added prefactored sis

--- main.cpp
+++ main.cpp
@@ -9,62 +9,12 @@
 
 using std::vector;
 using milli = std::chrono::milliseconds;
+using timer = std::chrono::high_resolution_clock;
 
 void print(const vector<int>& numbers)
 {
 	std::copy(begin(numbers), end(numbers), std::ostream_iterator<int>(std::cout, " "));
 	std::cout << '\n';
-}
-
-vector<int> selfIndexSort(vector<int> numbers)
-{
-	auto start = std::chrono::high_resolution_clock::now();
-
-	// Initialize Sort Space
-	int biggest_num = std::numeric_limits<int>::min(), smallest_num = std::numeric_limits<int>::max();
-	for (int num : numbers)
-	{
-		if (num < smallest_num)
-			smallest_num = num;
-		if (num > biggest_num)
-			biggest_num = num;
-	}
-	vector<int> sort_space(biggest_num - smallest_num + 1, 0);
-	
-	// Self-indexed arrangement	
-	for (int num : numbers)
-		sort_space[num - smallest_num]++;
-	
-	// Order-preserved compression
-	{
-		unsigned i = 0, SS_COUNT = sort_space.size();
-		for (unsigned j = 0; j < SS_COUNT; ++j)  // O(m)?
-			while (sort_space[j] > 0) 
-			{
-				numbers[i++] = j + smallest_num;
-				sort_space[j]--;
-			}
-	}
-
-	std::cout << std::setfill(' ') << std::setw(10) 
-		<< std::chrono::duration_cast<milli>(std::chrono::high_resolution_clock::now() - start).count() 
-		<< " ms - selfIndexSort\n";
-
-	return numbers;
-}
-
-vector<int> quickSort(vector<int> numbers)
-{
-	auto start = std::chrono::high_resolution_clock::now();
-
-	std::sort(begin(numbers), end(numbers));
-	auto finish = std::chrono::high_resolution_clock::now();
-
-	std::cout << std::setfill(' ') << std::setw(10) 
-		<< std::chrono::duration_cast<milli>(std::chrono::high_resolution_clock::now() - start).count() 
-		<< " ms - quickSort\n";
-
-	return numbers;
 }
 
 vector<int> generateNumbers(const unsigned COUNT, const int RANGE_START, const int RANGE_END)
@@ -82,25 +32,118 @@ vector<int> generateNumbers(const unsigned COUNT, const int RANGE_START, const i
 	return numbers;
 }
 
+vector<int> quickSort(vector<int> numbers)
+{
+	auto start = timer::now();
+	std::sort(begin(numbers), end(numbers));
+	std::cout << std::setfill(' ') << std::setw(10)
+		<< std::chrono::duration_cast<milli>(timer::now() - start).count()
+		<< " ms - quickSort\n";
+	return numbers;
+}
+
+vector<int> selfIndexSort(vector<int> numbers)
+{
+	auto start = timer::now();
+
+	// Initialize Sort Space
+	int biggest_num = std::numeric_limits<int>::min(), smallest_num = std::numeric_limits<int>::max();
+	for (int num : numbers)
+	{
+		if (num < smallest_num)
+			smallest_num = num;
+		if (num > biggest_num)
+			biggest_num = num;
+	}
+	const unsigned SS_COUNT = biggest_num - smallest_num + 1;
+	vector<int> sort_space(SS_COUNT, 0);
+
+	// Self-indexed arrangement	
+	for (int num : numbers)
+		sort_space[num - smallest_num]++;
+
+	// Order-preserved compression
+	{
+		unsigned i = 0;
+		for (unsigned j = 0; j < SS_COUNT; ++j)
+			while (sort_space[j] > 0)
+			{
+				numbers[i++] = j + smallest_num;
+				sort_space[j]--;
+			}
+	}
+
+	std::cout << std::setfill(' ') << std::setw(10)
+		<< std::chrono::duration_cast<milli>(timer::now() - start).count()
+		<< " ms - selfIndexSort\n";
+
+	return numbers;
+}
+
+vector<int> selfIndexSort(vector<int> numbers, int smallest_num, int biggest_num)
+{
+	auto start = timer::now();
+
+	const unsigned SS_COUNT = biggest_num - smallest_num + 1;
+	vector<int> sort_space(SS_COUNT, 0);
+
+	for (int num : numbers)
+		sort_space[num - smallest_num]++;
+
+	unsigned i = 0;
+	for (unsigned j = 0; j < SS_COUNT; ++j)
+		while (sort_space[j] > 0)
+		{
+			numbers[i++] = j + smallest_num;
+			sort_space[j]--;
+		}
+
+	std::cout << std::setfill(' ') << std::setw(10)
+		<< std::chrono::duration_cast<milli>(timer::now() - start).count()
+		<< " ms - selfIndexSort (pre-factored)\n";
+
+	return numbers;
+}
+
 void test(const unsigned COUNT, const int RANGE_START, const int RANGE_END)
 {
 	vector<int> unsorted_numbers = generateNumbers(COUNT, RANGE_START, RANGE_END);
+	int biggest_num = std::numeric_limits<int>::min(), smallest_num = std::numeric_limits<int>::max();
+	for (int num : unsorted_numbers)
+	{
+		if (num < smallest_num)
+			smallest_num = num;
+		if (num > biggest_num)
+			biggest_num = num;
+	}
+
 	std::cout << "Sorting...\n";
-	vector<int> sorted_sis_numbers = selfIndexSort(unsorted_numbers);
-	vector<int> sorted_qs_numbers = quickSort(unsorted_numbers);
+	selfIndexSort(unsorted_numbers);
+	selfIndexSort(unsorted_numbers, smallest_num, biggest_num);
+	quickSort(unsorted_numbers);
 
 	// Sanity check
-	assert(std::is_sorted(begin(sorted_sis_numbers), end(sorted_sis_numbers)));
-	assert(std::is_sorted(begin(sorted_qs_numbers), end(sorted_qs_numbers)));
-	for (unsigned i = 0; i < COUNT; ++i)
-		assert(sorted_sis_numbers[i] == sorted_qs_numbers[i]);
+	//vector<int> sorted_sis_numbers = selfIndexSort(unsorted_numbers);
+	//vector<int> sorted_sis_numbers2 = selfIndexSort(unsorted_numbers, smallest_num, biggest_num);
+	//vector<int> sorted_qs_numbers = quickSort(unsorted_numbers);
+
+	//assert(std::is_sorted(begin(sorted_sis_numbers), end(sorted_sis_numbers)));
+	//assert(std::is_sorted(begin(sorted_qs_numbers), end(sorted_qs_numbers)));
+	//assert(std::is_sorted(begin(sorted_sis_numbers2), end(sorted_sis_numbers2)));
+	//for (unsigned i = 0; i < COUNT; ++i)
+	//{
+	//	assert(sorted_sis_numbers[i] == sorted_qs_numbers[i]);
+	//	assert(sorted_sis_numbers2[i] == sorted_qs_numbers[i]);
+	//}
 }
 
 int main()
 {
-	test(  1'000'000,			 0,	100'000'000);
-	test(100'000'000,			 0,	100'000'000);
-	test(100'000'000,	-1'000'000,	  1'000'000);
+	test(1'000'000, 0, 1'000'000);
+	test(2'000'000, 0, 2'000'000);
+	test(3'000'000, 0, 3'000'000);
+	test(4'000'000, 0, 4'000'000);
+	test(5'000'000, 0, 5'000'000);
 
 	std::cout << "Press enter to exit...";
 	std::cin.get();
